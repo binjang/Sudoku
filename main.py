b7156b989fb4208e237a998adaec79ab9fa53ff9
binjang <bjang98@gmail.com>
2021-07-20 03:58:48 +0900

    In progress: Updating solver for cases when it has to make a guess

--- main.py
+++ main.py
@@ -87,6 +87,10 @@ def sudoku_solver(sudoku):
     missing_rows = find_missing_row(sudoku)
     missing_cols = find_missing_column(sudoku)
 
+    print(missing_squares)
+    print(missing_rows)
+    print(missing_cols)
+
     not_found = [] # List of indices of blanks
     not_found_pos = [] # List of possible nums for each blank
 
@@ -109,9 +113,19 @@ def sudoku_solver(sudoku):
 
         not_found_pos.append(pos)
 
+    cnt = 0
     while len(not_found) > 0:
+        cnt += 1
         for i in range(len(not_found)):
+            print(i, "th loop")
+            print("Blanks to fill: ", len(not_found))
+            print("no_found_pos", not_found_pos)
             if len(not_found_pos[i]) == 1:
+
+                print("Filling in blank: ", not_found[i][0])
+                print("Answer: ", not_found_pos[i][0])
+                print("Blanks left: ", len(not_found) - 1)
+
                 sudoku[not_found[i][0]] = not_found_pos[i][0] # Fill the blank with the only possible choice
 
                 for j in range(len(not_found)):
@@ -123,12 +137,32 @@ def sudoku_solver(sudoku):
 
                 not_found.pop(i) # Remove index from list
                 not_found_pos.pop(i) # Remove choice from list
-
                 break
+
+            elif len(not_found_pos[i]) == 2:
+                print("Guessing blank: ", not_found[i][0])
+                print("Guess: ", not_found_pos[i][0])
+                print("Blanks left: ", len(not_found) - 1)
+
+                sudoku[not_found[i][0]] = not_found_pos[i][0]  # Fill the blank with the only possible choice
+
+                for j in range(len(not_found)):
+                    is_pos = not_found_pos[j].count(not_found_pos[i][0]) != 0
+                    if not_found[j][1] == not_found[i][1] or not_found[j][2] == not_found[i][2] or not_found[j][3] == \
+                            not_found[i][3]:
+                        if is_pos:
+                            if i != j:
+                                not_found_pos[j].remove(not_found_pos[i][0])
+                not_found.pop(i)  # Remove index from list
+                not_found_pos.pop(i)  # Remove choice from list
+                break
+
+        if cnt == 100: break
 
     print("Sudoku solved!")
     for line_num in range(9):
         print(sudoku[9 * line_num: 9 * line_num + 9])
     print("--- %s seconds ---" % (time.time() - start_time))
+
 
 sudoku_solver(sudoku)
